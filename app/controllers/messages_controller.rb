7a477dddb664c12d5d0374a95e5be898b552a852
tlsmooth88 <tlsmooth88@gmail.com>
2016-01-09 04:03:39 +0000

    created message model

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,16 @@
 class MessagesController < ApplicationController
   def index
+    @message = Message.new
+  end
+  
+  def create
+    @message = Message.new(message_prarams)
+    @message.save
+    redirect_to root_path , notice: 'メッセージを保存しました'
+  end
+  
+  private
+  def message_prarams
+    params.require(:message).permit(:name, :body)
   end
 end
