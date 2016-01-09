564058648d40e162a22f57942646c320d817afbe
tlsmooth88 <tlsmooth88@gmail.com>
2016-01-09 06:28:59 +0000

    finalized as a part of my portfolio

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,13 +1,15 @@
 class MessagesController < ApplicationController
+  before_action :set_message, only: [:edit, :update, :destroy]
+  
   def index
     @message = Message.new
     @messages = Message.all
   end
   
   def create
-    @message = Message.new(message_prarams)
+    @message = Message.new(message_params)
     if @message.save
-      redirect_to root_path , notice: 'Your message is savied.'
+      redirect_to root_path , notice: 'Your message is saved.'
     else
       @messages = Message.all
       flash.now[:alert] = "Your massage failed to save."
@@ -15,8 +17,30 @@ class MessagesController < ApplicationController
     end
   end
   
+  def edit
+  end
+  
+  def update
+    if @message.update(message_params)
+      redirect_to root_path , notice: 'Message updated.'
+    else
+      render 'edit'
+    end
+  end
+  
+  def destroy
+    @message.destroy
+    redirect_to root_path, notice: 'Message deleted.'
+  end
+  
   private
-  def message_prarams
-    params.require(:message).permit(:name, :body)
+  def message_params
+    params.require(:message).permit(:name, 
+    #:age,
+    :body)
+  end
+  
+  def set_message
+    @message = Message.find(params[:id])
   end
 end
