42fedacef2f9a7ff33531a0ce267182274205441
tlsmooth88 <tlsmooth88@gmail.com>
2016-01-09 05:39:25 +0000

    Validation implemented

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -6,8 +6,13 @@ class MessagesController < ApplicationController
   
   def create
     @message = Message.new(message_prarams)
-    @message.save
-    redirect_to root_path , notice: 'メッセージを保存しました'
+    if @message.save
+      redirect_to root_path , notice: 'Your message is savied.'
+    else
+      @messages = Message.all
+      flash.now[:alert] = "Your massage failed to save."
+      render 'index'
+    end
   end
   
   private
